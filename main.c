10fc41b503326aefee77b194a1f0538d6765d2e1
Tomaz Solc <tomaz.solc@tablix.org>
2013-05-17 15:28:42 +0200

    Collate events with same timestamp.

--- main.c
+++ main.c
@@ -227,23 +227,29 @@ int main(void)
 
 		while(cur_event < events_num) {
 
-			unsigned tw = vss_dds_get_tuning_word(fs, events[cur_event].freq);
+			unsigned cur_time = events[cur_event].time;
 
-			unsigned i = 0;
-			if(events[cur_event].type) {
-				while(tw_list[i] != 0 && i < tw_num-1) i++;
-				tw_list[i] = tw;
-			} else {
-				while(tw_list[i] != tw && i < tw_num-1) i++;
-				tw_list[i] = 0;
+			while(events[cur_event].time == cur_time && cur_event < events_num) { 
+				unsigned tw = vss_dds_get_tuning_word(fs, events[cur_event].freq);
+
+				unsigned i = 0;
+				if(events[cur_event].type) {
+					while(tw_list[i] != 0 && i < tw_num-1) i++;
+					tw_list[i] = tw;
+				} else {
+					while(tw_list[i] != tw && i < tw_num-1) i++;
+					tw_list[i] = 0;
+				}
+
+				printf("ev %d %u -> %u %u %u\n", i, tw, tw_list[0], tw_list[1], tw_list[2]);
+
+				cur_event++;
 			}
-
-			printf("ev %d %u -> %u %u %u\n", i, tw, tw_list[0], tw_list[1], tw_list[2]);
 
 			vss_dds_fill_poly(backbuffer, sizeof(dds_buffer_1), &output, tw_list, tw_num);
 
 			int waits = 0;
-			while(events[cur_event].time > vss_rtc_read()) waits++;
+			while(cur_time > vss_rtc_read()) waits++;
 			if(waits == 0) {
 				printf("OVERTIME!\n");
 			}
@@ -251,8 +257,6 @@ int main(void)
 			dds_t *t = dds_buffer;
 			dds_buffer = backbuffer;
 			backbuffer = t;
-
-			cur_event++;
 		}
 	}
 }
