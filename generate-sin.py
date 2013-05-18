c70dbee74e8971e09abb4baefe2bac5113cb63c6
Tomaz Solc <tomaz.solc@tablix.org>
2013-05-18 21:51:07 +0200

    Add harmonics to wavetable.

--- generate-sin.py
+++ generate-sin.py
@@ -13,7 +13,10 @@ for n in xrange(length):
 	if n % 8 == 0:
 		sys.stdout.write("\n\t")
 
-	v = math.cos(2.0*math.pi*float(n)/float(length))
+	v = math.cos(2.0*math.pi*float(n)/float(length))*0.8/1.5
+	v += math.cos(4.0*math.pi*float(n)/float(length))*0.4/1.5
+	v += math.cos(6.0*math.pi*float(n)/float(length))*0.2/1.5
+	v += math.cos(8.0*math.pi*float(n)/float(length))*0.1/1.5
 
 	c = v * 127.0
 
